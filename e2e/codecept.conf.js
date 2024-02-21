afedf09093da99040c4a4cf204623e4a184a6042
Vinicius <vsdiaz86@hotmail.com>
2024-02-21 15:36:46 -0300

    Add files via upload

---
+++ e2e/codecept.conf.js
@@ -0,0 +1,67 @@
+const dotenv = require('dotenv');
+const dotenvParseVariables = require('dotenv-parse-variables');
+
+let env = dotenv.config()
+if (env.error) throw env.error;
+env = dotenvParseVariables(env.parsed);
+
+exports.config = {
+	output: './output',
+	helpers: {
+        Playwright: {
+            url: process.env.BASE_URL,
+            show: process.env.SHOW_BROWSER === 'true',
+            browser: process.env.BROWSER,
+            //windowSize: process.env.WINDOW_SIZE,
+            waitForNavigation: "domcontentloaded",
+            waitForAction: 700,
+            chromium: {
+                args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage',
+                    '--ignore-certificate-errors']
+            },
+            firefox: {
+                args: ['--no-sandbox', '--ignore-certificate-errors']
+            }
+		},
+		CustomHelper: {
+			require: './support/helpers/custom_helper.js',
+		},
+		ResembleHelper : {
+			require: "codeceptjs-resemblehelper",
+			screenshotFolder : "./output/",
+			baseFolder: "./support/screenshots/base/",
+			diffFolder: "./support/screenshots/diff/"
+		  },
+		REST: {}
+	},
+	include: {
+		I: './steps_file.js',
+	},
+	async bootstrapAll() {	
+	},
+	async teardownAll() {
+	},
+	mocha: {},
+	bootstrap: null,
+	teardown: null,
+	hooks: [],
+	gherkin: {
+		features: './features/*/*.feature'
+	},
+	plugins: {
+		allure: {
+			enabled: true,
+			require: '@codeceptjs/allure-legacy',  
+			outputDir: 'output'
+		},
+		screenshotOnFail: {
+			enabled: true
+		},
+		skipper: {
+			require: './support/plugins/skipper.js',
+			enabled: true
+		}
+	},
+	tests: './step_definitions/*/*_test.js',
+	name: 'e2e'
+}
